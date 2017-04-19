e366833e9f0dab4cf419230f9e17acb076020dfa
Carlos Rosão <carlos.rosao@plusgrade.com>
2017-04-19 20:05:03 +0100

    Improves functional testing

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
 
-assert 'Django' in browser.title
+class MainPageTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_open_main_page(self):
+        self.browser.get('http://127.0.0.1:8000')
+
+        self.assertIn('This Day in Music', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
