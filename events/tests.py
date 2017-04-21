2dd9828407fb7e99995f090a7594c6f03617388f
Carlos Rosão <carlos.rosao@plusgrade.com>
2017-04-21 10:08:43 +0100

    Homepage view now uses a template

--- events/tests.py
+++ events/tests.py
@@ -1,17 +1,7 @@
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
 from django.test import TestCase
-
-from events.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-    def test_homepage_returns_html(self):
-        request = HttpRequest()
-
-        response = home_page(request)
-        self.assertEqual(response.status_code, 200)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,17 @@ class MainPageTest(unittest.TestCase):
 
         self.assertIn('This Day in Music', self.browser.title)
 
+        brand_text = self.browser.find_element_by_class_name(
+                'navbar-brand'
+                ).text
+        self.assertIn('This Day In Music', brand_text)
+
+        header = self.browser.find_element_by_tag_name('h1')
+        header_description = header.find_element_by_tag_name('small')
+        self.assertIn(
+            'Events that happened on this day in music...', header_description
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
