62e8f4523b9646a3831822bd7fea98aa9eaff5cd
Carlos Rosão <carlos.rosao@plusgrade.com>
2017-04-26 21:43:19 +0100

    Tests event page calling event service with the right args

--- events/tests.py
+++ events/tests.py
@@ -9,10 +9,24 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     @mock.patch('events.services.EventService.EventService.events')
-    def test_call_event_service(self, mock_event_service_get):
+    def test_home_page_today_calls_event_service(self, mock_event_service_get):
         self.client.get('/')
 
         self.assertTrue(mock_event_service_get.called)
+
+    @mock.patch('events.services.EventService.EventService.events')
+    def test_events_page_calls_event_service(self, mock_event_service_get):
+        # when I access a random events page day
+        self.client.get('/events/April/25')
+
+        # then the service is called with the right arguments
+        self.assertIn(
+            mock.call(
+                'April',
+                '25'
+            ),
+            mock_event_service_get.call_args_list
+        )
 
 
 class EventServiceTest(TestCase):
