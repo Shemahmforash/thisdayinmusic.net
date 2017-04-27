712377b6f205e0b659d7dfc81cf2e3769f0c64f5
Carlos Rosão <carlos.rosao@plusgrade.com>
2017-04-27 11:03:13 +0100

    Let date be supplied to the template.

--- events/views.py
+++ events/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from events.services.EventService import EventService
+from datetime import datetime
 
 
 def home_page(request):
     events = EventService.events()
+    date = datetime.now()
 
     return render(request, 'home.html', {
         'events': events['response']['events'],
+        'date': date,
     })
 
 
 def events_page(request, month, day):
     events = EventService.events(month, day)
+    date = datetime.strptime(
+            '{} {}'.format(day, month), '%d %B')
 
     return render(request, 'home.html', {
         'events': events['response']['events'],
+        'date': date,
     })
