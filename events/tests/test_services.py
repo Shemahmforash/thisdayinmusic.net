ee55a973ea7e88f9c9452b02348ec05213daf4de
Carlos Rosão <carlos.rosao@siroop.ch>
2017-06-24 11:18:30 +0100

    Splits tests to different files in new package
    Adds a new package just for the different unit tests

--- events/tests/test_services.py
+++ events/tests/test_services.py
@@ -1,37 +1,11 @@
-from django.test import TestCase
 from unittest import mock
+
+from django.test import TestCase
+
 from events.services.EventService import EventService
 
 
-class HomePageTest(TestCase):
-
-    def test_uses_home_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
-    @mock.patch('events.services.EventService.EventService.events')
-    def test_home_page_today_calls_event_service(self, mock_event_service_get):
-        self.client.get('/')
-
-        self.assertTrue(mock_event_service_get.called)
-
-    @mock.patch('events.services.EventService.EventService.events')
-    def test_events_page_calls_event_service(self, mock_event_service_get):
-        # when I access a random events page day
-        self.client.get('/events/April/25')
-
-        # then the service is called with the right arguments
-        self.assertIn(
-            mock.call(
-                'April',
-                '25'
-            ),
-            mock_event_service_get.call_args_list
-        )
-
-
 class EventServiceTest(TestCase):
-
     @mock.patch('events.services.EventService.requests.get')
     def test_events_with_no_args_api_is_called_with_no_args(
             self, requests_mock):
